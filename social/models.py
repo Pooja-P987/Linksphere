e249a818d678d70e64e599f1064d8dfe3d801660
Pooja-P987 <poojapremarajan987@gmail.com>
2023-12-22 13:33:22 +0530

    linksphere app

--- social/models.py
+++ social/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+# Create your models here.from django.db import models
+
+from django.contrib.auth.models import User
+
 from datetime import datetime
+
 from django.utils import timezone
+
+from django.utils import timezone
+
+from django.db.models.signals import post_save
 
 # Create your models here.
 class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
-    address=models.CharField(max_length=200)
-    phone=models.CharField(max_length=200)
+    address=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
     profile_pic=models.ImageField(upload_to="profilepics",null=True,blank=True)
-    dob=models.DateField()
-    bio=models.CharField(max_length=200)
-    following=models.ManyToManyField("self",related_name="followed_by",symmetrical=False)
-    block=models.ManyToManyField("self",related_name="blocked",symmetrical=False)
+    dob=models.DateField(null=True)
+    bio=models.CharField(max_length=200,null=True)
+    following=models.ManyToManyField("self",related_name="followed_by",symmetrical=False,null=True)
+    block=models.ManyToManyField("self",related_name="blocked",symmetrical=False,null=True)
 
 
     def __str__(self):
@@ -22,7 +32,7 @@ class Posts(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="userpost")
     title=models.CharField(max_length=200)
     post_image=models.ImageField(upload_to="posters",null=True,blank=True)
-    created_date=models.DateField(auto_now_add=True)
+    created_date=models.DateTimeField(auto_now_add=True)
     liked_by=models.ManyToManyField(User,related_name="post_like")
 
     def __str__(self):
@@ -34,17 +44,34 @@ class Comments(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     post=models.ForeignKey(Posts,related_name="post_comments",on_delete=models.CASCADE)
 
+
     def __str__(self):
         return self.text
+    
 
 class Stories(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="userstories")
     title=models.CharField(max_length=200)
     post_image=models.ImageField(upload_to="stories",null=True,blank=True)
-    created_date=models.DateField(auto_now_add=True)
-    exp=created_date + timezone.timedelta(days=1)
-    expiry_date=models.DateTimeField(exp)
+    created_date=models.DateTimeField(auto_now_add=True)
+    # exp=created_date + timezone.timedelta(days=1)
+    expiry_date=models.DateTimeField()
+    
 
-    def __str__(self):
+    def str(self):
         return self.title
     
+    def save(self,*args,**kwargs):
+        if not self.expiry_date:
+            self.expiry_date=timezone.now()+timezone.timedelta(days=1)
+        super().save(*args,**kwargs)
+
+
+
+    
+    
+def create_profile(sender,created,instance,**kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+post_save.connect(create_profile,sender=User)
